9cef29dd774f608265145dc168ede38c52c4c27d
Исанов Аслан Казбулатович <IsanovAs@lcoast.itstep>
2023-08-23 17:56:39 +0600

    aslan

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,44 +1,35 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import './Search.css';
+import "./Search.css";
 export default function Search() {
+  const navigate = useNavigate();
+  const [cityName, setCityName] = useState("");
+  const savedHistory = localStorage.getItem("cityHistory")
+    ? localStorage.getItem("cityHistory").json.parse()
+    : [];
 
-    const navigate = useNavigate();
-    const [cityName, setCityName] = useState('');
-    const [cityHistory, setCityHistory] = useState([]);
-
-    useEffect(() => {
-        const savedHistory = localStorage.getItem("cityHistory")
-        if (savedHistory) {
-            setCityHistory(JSON.parse(savedHistory));
-        }
-    }, []);
-
-    const onClick = () => {
-        if (cityName.trim() === '') {
-            return;
-        }
-       
-        const updatedHistory = [...cityHistory, cityName];
-        setCityHistory(updatedHistory);
-        localStorage.setItem("cityHistory", JSON.stringify(updatedHistory));
-        navigate(`/${cityName}`);
-    };
-    const handleChange = e => {
-        setCityName(e.target.value);
-    };
-
-
-
-    return (
-        <div className="inputStyle">
-            <input
-                placeholder="Напишите город"
-                type='text'
-                value={cityName}
-                onChange={handleChange}
-            />
-            <button disabled={cityName === ""} onClick={onClick}>Find</button>
-        </div>
-    );
+  const onClick = () => {
+    if (cityName.trim() === "") {
+      return;
+    }
+    const updatedHistory = [...savedHistory, cityName];
+    localStorage.setItem("cityHistory", JSON.stringify(updatedHistory));
+    navigate(`/${cityName}`);
+  };
+  const handleChange = (e) => {
+    setCityName(e.target.value);
+  };
+  return (
+    <div className="inputStyle">
+      <input
+        placeholder="Напишите город"
+        type="text"
+        value={cityName}
+        onChange={handleChange}
+      />
+      <button disabled={cityName === ""} onClick={onClick}>
+        Find
+      </button>
+    </div>
+  );
 }
